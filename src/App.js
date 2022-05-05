c200e409059b81055170d6231c9a7715f4ea8819
Pat Lawler <patlaw777@icloud.com>
2022-05-04 21:15:24 -0500

    added contact form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
       />
       <main>
         <div>
+          <ContactForm />
           <Gallery currentCategory = { currentCategory }/>
           <About />
         </div>
